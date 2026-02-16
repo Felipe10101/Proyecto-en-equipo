f4a5bdb99c346343d71736042510363214b1adc5
Juan4812 <juanj.riveram1@uqvirtual.edu.co>
2026-02-15 20:21:57 -0500

    Update Main.java

--- src/main/java/co/edu/uniquindio/poo/Main.java
+++ src/main/java/co/edu/uniquindio/poo/Main.java
@@ -8,12 +8,11 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("Bienvenido al sistema!");
 
-        for (int i = 1; i <= 6; i++) {
+        for (int i = 1; i <= 11; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
-        }
 
-        //Hola
+        }
     }
 }
